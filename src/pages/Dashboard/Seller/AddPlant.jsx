4cda9be85ffe478001ff3a0ef5e2d2b42f6c6d55
shahidul shohid <shahidulislamshohid7gmail.com>
2025-01-03 17:17:26 +0600

    addplants page is done

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -1,7 +1,51 @@
 import { Helmet } from 'react-helmet-async'
 import AddPlantForm from '../../../components/Form/AddPlantForm'
+import { imageUpload } from '../../../api/utils'
+import useAuth from '../../../hooks/useAuth'
+import { useState } from 'react'
+import useAxiosSecure from '../../../hooks/useAxiosSecure'
+import toast from 'react-hot-toast'
 
 const AddPlant = () => {
+  const {user} = useAuth()
+  const axiosSecure = useAxiosSecure()
+
+  const [uploadButtonText, setUploadButtonText] = useState({name:'Upload Button'})
+  const [loading, setLoading] = useState(false)
+  const handleSubmit = async(e) => {
+    setLoading(true)
+    e.preventDefault()
+    const form = e.target 
+    const name = form.name.value 
+    const description = form.description.value
+    const category = form.category.value 
+    const price = parseFloat(form.price.value)
+    const quantity = parseInt(form.quantity.value)
+    const image = form.image.files[0]
+    const imageUrl = await imageUpload(image)
+    // user info 
+    const seller = {
+      name:user?.displayName,
+      email:user?.email,
+      image:user?.photoURL,
+    }
+
+    //create plant data object
+    const plantData ={name,description,category,price, quantity,image:imageUrl, seller
+    }
+
+    // save plant in db
+    try {
+      // post request 
+      await axiosSecure.post('/plants', plantData)
+      toast.success('Data added successfully!')
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setLoading(false)
+    }
+  }
   return (
     <div>
       <Helmet>
@@ -9,7 +53,7 @@ const AddPlant = () => {
       </Helmet>
 
       {/* Form */}
-      <AddPlantForm />
+      <AddPlantForm handleSubmit={handleSubmit} uploadButtonText={uploadButtonText} setUploadButtonText={setUploadButtonText} loading={loading}/>
     </div>
   )
 }
